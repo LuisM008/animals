5efc8de002911c60f59a1e0bf9af588e7f1da9ab
LuisM008 <luiseduardo.monico@upaep.mx>
2026-04-23 14:27:03 +0000

    generacion de todo el codigo

--- Perro.cs
+++ Perro.cs
@@ -1,1 +1,35 @@
+class Perro : Animal
+{
+    //Atributo propio de Perro
+    public string Raza;
+    //Constructor
+    public Perro(string nombre,int edad,string raza) 
+    : base (nombre,edad)
+    {
+        Raza = raza ;
+    }
 
+    //Sobreescribe el metodo padre
+    public new void HacerSonido()
+    {
+        Console.WriteLine(Nombre+"dice ¡Guau!");
+    }
+    //Metodo propio de Perro
+    public void Buscar()
+    {
+        Console.WriteLine(Nombre+" esta buscando algo...");
+    } 
+}
+
+class Program
+{
+    static void Main()
+    {
+        Perro miPerro= new Perro("Gorda",2,"Pastor Belga");
+        miPerro.MostarInfo();
+        miPerro.HacerSonido();
+        miPerro.Buscar();
+
+        Console.WriteLine("Raza:"+ miPerro.Raza);
+    }
+}
